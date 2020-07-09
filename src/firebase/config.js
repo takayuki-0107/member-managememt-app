e9e8d8f2a597cf9e298b8d998afe8c8561761097
Takayuki Tooyama <t.takayuki.0107@gmail.com>
2020-07-09 17:58:10 +0900

    firebase初期設定

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDT6NEW1CAEvgGDeK-OrN73L9Qy_N6Gq6U',
+  authDomain: 'member-management-app.firebaseapp.com',
+  databaseURL: 'https://member-management-app.firebaseio.com',
+  projectId: 'member-management-app',
+  storageBucket: 'member-management-app.appspot.com',
+  messagingSenderId: '727024611912',
+  appId: '1:727024611912:web:cd367febfa008c2d7e9e7d',
+  measurementId: 'G-7C2L4K7C18',
+};
---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/functions';
+import 'firebase/storage';
+import { firebaseConfig } from './config';
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export const functions = firebase.functions();
+export const storage = firebase.storage();
+export const FirebaseTimestamp = firebase.firestore.Timestamp;
